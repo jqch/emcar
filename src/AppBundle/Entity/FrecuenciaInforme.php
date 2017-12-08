7711c6a4c552a244b5ae30b62a9ae7f43403fd14
Jhonny <jhonnyq17@gmail.com>
2017-12-08 18:51:39 -0400

    Mappind de base y creacion de cruds

---
+++ src/AppBundle/Entity/FrecuenciaInforme.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * FrecuenciaInforme
+ */
+class FrecuenciaInforme
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $frecuenciaInforme;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set frecuenciaInforme
+     *
+     * @param string $frecuenciaInforme
+     *
+     * @return FrecuenciaInforme
+     */
+    public function setFrecuenciaInforme($frecuenciaInforme)
+    {
+        $this->frecuenciaInforme = $frecuenciaInforme;
+
+        return $this;
+    }
+
+    /**
+     * Get frecuenciaInforme
+     *
+     * @return string
+     */
+    public function getFrecuenciaInforme()
+    {
+        return $this->frecuenciaInforme;
+    }
+}
+
